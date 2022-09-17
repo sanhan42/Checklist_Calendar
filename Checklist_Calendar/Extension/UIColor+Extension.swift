44f426442ca43622e79c6687467b5eedb8b75f2b
sanhan42 <sangmin613@gmail.com>
2022-09-17 10:18:55 +0900

    Add UIColor to String & String to UIColor Func

--- Checklist_Calendar/Extension/UIColor+Extension.swift
+++ Checklist_Calendar/Extension/UIColor+Extension.swift
@@ -8,7 +8,39 @@
 import UIKit
 
 extension UIColor {
-    class var bgColor: UIColor? { return UIColor(named: "BgColor")}
-    class var shGrayColor: UIColor? { return UIColor(named: "GreyColor")}
-    class var textColor: UIColor? { return UIColor.black.withAlphaComponent(0.9)}
+    // 사용자 정의 색
+    class var bgColor: UIColor { return UIColor(named: "BgColor")!}
+    class var shGrayColor: UIColor { return UIColor(named: "GreyColor")!}
+    class var textColor: UIColor { return UIColor.black.withAlphaComponent(0.9)}
+
+    // UIColor <-> String (DB에 color를 저장하기 위해 String으로 바꿔줄 필요가 있음.) (비트연산 사용.)
+    convenience init(hexAlpha: String) {
+        var hexFormatted: String = hexAlpha.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 8, "Invalid hex code used.") // 반드시 8 자리여야 함.
+
+        var rgbaValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbaValue)
+
+        self.init(red: CGFloat((rgbaValue & 0xFF000000) >> 24) / 255.0,
+                  green: CGFloat((rgbaValue & 0x00FF0000) >> 16) / 255.0,
+                  blue: CGFloat((rgbaValue & 0x0000FF00) >> 8) / 255.0,
+                  alpha: CGFloat(rgbaValue & 0x000000FF) / 255.0)
+    }
+
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        print(r, g, b, a)
+        let rgba:Int = (Int)(r*255)<<24 | (Int)(g*255)<<16 | (Int)(b*255)<<8 | (Int)(a*255)<<0
+
+        return String(format:"#%08x", rgba)
+    }
 }
