85c9704c4b34a69f39ffa54fa03445bda230c1ed
sanhan42 <sangmin613@gmail.com>
2022-09-17 10:33:04 +0900

    Edit Realm Data Model

--- Checklist_Calendar/Realm/Event.swift
+++ Checklist_Calendar/Realm/Event.swift
@@ -10,19 +10,23 @@ import RealmSwift
 
 class Event: Object {
     @Persisted var title: String
-    @Persisted var color: Int?
-    @Persisted var startDate: Date?
-    @Persisted var endDate: Date?
+    @Persisted var color: String
+    @Persisted var startDate = Date()
+    @Persisted var startTime: Date?
+    @Persisted var endDate = Date()
+    @Persisted var endTime: Date?
     @Persisted var isAllDay: Bool
     @Persisted(primaryKey: true) var id: ObjectId
     let todos = List<Todo>()
     
-    convenience init(title: String, color: Int? = 0, startDate: Date?, endDate: Date?, isAllDay: Bool) {
+    convenience init(title: String, color: String, startDate: Date, starTime: Date?, endDate: Date, endTime: Date?, isAllDay: Bool) {
         self.init()
         self.title = title
         self.color = color
         self.startDate = startDate
+        self.startTime = startTime
         self.endDate = endDate
+        self.endTime = endTime
         self.isAllDay = isAllDay
     }
     
